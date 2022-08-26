dce4444158c8b0deaed4f18b352d2caf271037e1
Jona Kuhn <jona.kuhn@dm.de>
2022-08-26 13:01:28 +0200

    update link to PDS

--- components/Products.jsx
+++ components/Products.jsx
@@ -10,7 +10,7 @@ const Products = (props) => {
             {
                 productData.products.map(element => {
                     return (
-                        <a href={`https://www.dm.de/search?query=${element.gtin}&searchType=product`} key={element.id}>
+                        <a href={`https://www.dm.de/p${element.gtin}.html`} key={element.id}>
                             <div className={styles.productPreview}>
                                 <img className={styles.productImg} src={getImageUrl(element.imageUrlTemplates)} layout='fill'
                                     objectFit='contain' />
