a09b7abc99d8587e1ef97af7a54b8e438892fa1d
Jona Kuhn <jona.kuhn@dm.de>
2022-08-26 11:50:54 +0200

    Fix: added key

--- components/Products.jsx
+++ components/Products.jsx
@@ -10,7 +10,7 @@ const Products = (props) => {
             {
                 productData.products.map(element => {
                     return (
-                        <a href={`https://www.dm.de/search?query=${element.gtin}&searchType=product`}>
+                        <a href={`https://www.dm.de/search?query=${element.gtin}&searchType=product`} key={element.id}>
                             <div className={styles.productPreview}>
                                 <img className={styles.productImg} src={getImageUrl(element.imageUrlTemplates)} layout='fill'
                                     objectFit='contain' />
